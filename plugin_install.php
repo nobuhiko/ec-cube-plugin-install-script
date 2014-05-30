1eddb25d1afe28316d6d301301112312af95dafc
のぶ <nobuhiko.kimoto@gmail.com>
2014-05-30 18:59:46 +0900

    1ファイルに纏める

--- plugin_install.php
+++ plugin_install.php
@@ -21,10 +21,70 @@
 
 $path = realpath('../html/require.php');
 require_once($path);
+require_once CLASS_REALDIR . 'pages/admin/ownersstore/LC_Page_Admin_OwnersStore.php';
 
-$d = dir(constant("PLUGIN_UPLOAD_REALDIR"));
-while (false !== ($filename = $d->read())) {
-    if ($filename != '.' && $filename != '..'){
-        system("php script.php $filename");
+if (isset($argv[1])) {
+    print_r(installPlugin($argv[1]));
+} else {
+    $d = dir(constant("PLUGIN_UPLOAD_REALDIR"));
+    while (false !== ($filename = $d->read())) {
+        if ($filename != '.' && $filename != '..'){
+            system("php plugin_install.php $filename");
+        }
     }
+}
+
+
+function installPlugin($key) {
+
+    $objQuery =& SC_Query_Ex::getSingletonInstance();
+    $objQuery->begin();
+    $plugin_dir_path = PLUGIN_UPLOAD_REALDIR . $key . '/';
+
+    $objOwner = new LC_Page_Admin_OwnersStore();
+    // plugin_infoを読み込み.
+    $arrErr = $objOwner->requirePluginFile($plugin_dir_path . 'plugin_info.php', $key);
+    if ($objOwner->isError($arrErr) === true) {
+        return $arrErr;
+    }
+
+    // リフレクションオブジェクトを生成.
+    $objReflection = new ReflectionClass('plugin_info');
+    $arrPluginInfo = $objOwner->getPluginInfo($objReflection);
+    // プラグインクラスに必須となるパラメータが正常に定義されているかチェックします.
+    $arrErr = $objOwner->checkPluginConstants($objReflection, $plugin_dir_path);
+    if ($objOwner->isError($arrErr) === true) {
+        return $arrErr;
+    }
+
+    // 既にインストールされていないかを判定.
+    if ($objOwner->isInstalledPlugin($arrPluginInfo['PLUGIN_CODE']) === true) {
+        return '※ ' . $arrPluginInfo['PLUGIN_NAME'] . 'は既にインストールされています。' . "\n";
+    }
+
+    // プラグイン情報をDB登録
+    if ($objOwner->registerData($arrPluginInfo) === false) {
+        return '※ DB登録に失敗しました。' . "\n";
+    }
+
+    // プラグイン情報を取得
+    $plugin = SC_Plugin_Util_Ex::getPluginByPluginCode($arrPluginInfo['PLUGIN_CODE']);
+
+    // クラスファイルを読み込み.
+    $plugin_class_file_path = $objOwner->getPluginFilePath($plugin['plugin_code'], $plugin['class_name']);
+    $arrErr = $objOwner->requirePluginFile($plugin_class_file_path, $key);
+    if ($objOwner->isError($arrErr) === true) {
+        return $arrErr;
+    }
+    // プラグインhtmlディレクトリ作成
+    $plugin_html_dir_path = $objOwner->getHtmlPluginDir($plugin['plugin_code']);
+    $objOwner->makeDir($plugin_html_dir_path);
+
+    $arrErr = $objOwner->execPlugin($plugin, $plugin['class_name'], 'install');
+    if ($objOwner->isError($arrErr) === true) {
+        return $arrErr;
+    }
+
+    $objQuery->commit();
+    return $arrPluginInfo['PLUGIN_NAME'] . 'のインストールに成功しました。' . "\n";
 }
